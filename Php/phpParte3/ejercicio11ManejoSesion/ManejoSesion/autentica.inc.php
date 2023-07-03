87709ff39e5f873772f0bcc609e27fcbccceedbb
Facundo Ernesto Ortega <ortegafacundo955@gmail.com>
2023-07-03 17:07:14 -0300

    ultimos cambios en el sesion

--- Php/phpParte3/ejercicio11ManejoSesion/ManejoSesion/autentica.inc.php
+++ Php/phpParte3/ejercicio11ManejoSesion/ManejoSesion/autentica.inc.php
@@ -43,4 +43,43 @@
 
         return $resultado;
     }
+
+    function sumarIngresoSesion($log) {
+        $host = "b5oonaccrh45cpi3gevs-mysql.services.clever-cloud.com";
+        $dbname = "b5oonaccrh45cpi3gevs";
+        $user = "ugjhggs4vvwnlhjj";
+        $password = "QlXtbUgOrjZTNoFZ1VNm";
+
+        try {
+            $dsn = "mysql:host=$host;dbname=$dbname";
+            $dbh = new PDO($dsn, $user, $password);
+            $respuesta_estado = "\nConexion exitosa";
+        } catch (PDOExcepction $ex) {
+            $respuesta_estado = $ex->getMessage();
+        }
+
+        $sql = "SELECT ingresos FROM login_usuario WHERE usuario = :usuario";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(":usuario", $log);
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        $aux = $stmt->fetch();
+
+        $ingresos = implode(" ", $aux);
+        $ingresos++;
+
+        $sql = "UPDATE `login_usuario` SET ingresos = :ingresos WHERE usuario = :usuario";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(":ingresos", $ingresos);
+        $stmt->bindParam(":usuario", $log);
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        $aux = $stmt->fetch();
+
+        return $ingresos;
+    }
 ?>
--- Php/phpParte3/ejercicio11ManejoSesion/ManejoSesion/ingresoAlSistema.php
+++ Php/phpParte3/ejercicio11ManejoSesion/ManejoSesion/ingresoAlSistema.php
@@ -6,7 +6,8 @@
     <title>Menú Sesión</title>
 </head>
 <?php
-    include("./autentica.inc.php");
+    session_start();
+    require("./autentica.inc.php");
     
     $log = $_POST["usuario"];
     $contra = $_POST["contra"];
@@ -15,22 +16,22 @@
         header("location:./formularioIngreso.html");
         exit();
     } else {
-        session_start();
-        $_SESSION["idSesion"] = session_create_id();
-        $_SESSION["login"] = $log;
-        $_SESSION["contador"] = $_SESSION["contador"] + 1;
-
-        if (!isset($_SESSION["idSesion"])) {
-            header("location:./index.php");
-            exit();
+        if (!isset($_SESSION["idSesion"])) {        
+            $_SESSION["idSesion"] = session_create_id();
+            $_SESSION["login"] = $log;
+            $_SESSION["ingresos"] = sumarIngresoSesion($log);
+            $_SESSION["fechaIngreso"] = date("d-m-Y h:i:s");
+            $_SESSION["pais"] = "Argentina";
         }
 
         echo "
             <h1>Acceso Permitido al Sistema</h1>
             <h2>Los parámetro de su sesión son los siguientes:</h2>
-            <h3>ID de sesión: . " . $_SESSION["idSesion"] . "</h3>
-            <h3>Usuario: . " . $_SESSION["login"] . "</h3>
-            <h3>Contador de sesión: " . $_SESSION["contador"] . "</h3>
+            <h3>ID de sesión: " . $_SESSION["idSesion"] . "</h3>
+            <h3>Ingresos al sistema: " . $_SESSION["ingresos"] . "</h3>
+            <h3>Usuario: " . $_SESSION["login"] . "</h3>
+            <h3>Ingreso de la sesión: " . $_SESSION["fechaIngreso"] . "</h3>
+            <h3>País de logueo: " . $_SESSION["pais"] . "</h3>
         ";
 
         echo "<p><button onClick=\"location.href='../ABM'\">Ingrese a la aplicación</button></p>";
